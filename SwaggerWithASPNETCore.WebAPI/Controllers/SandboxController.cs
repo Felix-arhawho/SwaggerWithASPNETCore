8ce1adfa660c0adac7d61a90ad539dc465c0bbfb
Felix Arhawho <Felix.Arhawho@fcmb.com>
2022-04-12 15:53:07 +0100

    Successfully configured swagger

---
+++ SwaggerWithASPNETCore.WebAPI/Controllers/SandboxController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SwaggerWithASPNETCore.WebAPI.Dtos;
+using System;
+
+namespace SwaggerWithASPNETCore.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SandboxController : ControllerBase
+    {
+        private readonly ILogger<SandboxController> _logger;
+        public SandboxController(ILogger<SandboxController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("GetInitiatedRequestById")]
+        public ActionResult GetInitiatedRequestById(int Id)
+        {
+            try
+            {
+                //This code was delibrately written to throw exception
+                var myIntegerArray = new int[] { 3, 4, 12, 4 };
+                var myNumber = myIntegerArray[5];
+
+                return Ok(myNumber);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                _logger.LogError(ex.InnerException == null ? "" : ex.InnerException.ToString());
+                _logger.LogError(ex.StackTrace);
+
+                return BadRequest("An error occurred");
+            }
+        }
+
+        [HttpPost("InitiatedRequest")]
+        public ActionResult InitiatedRequest(RequestDto requestDto)
+        {
+            try
+            {
+                return Ok(requestDto);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                _logger.LogError(ex.InnerException == null ? "" : ex.InnerException.ToString());
+                _logger.LogError(ex.StackTrace);
+
+                return BadRequest("An error occurred");
+            }
+        }
+    }
+}
---
+++ SwaggerWithASPNETCore.WebAPI/Dtos/RequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SwaggerWithASPNETCore.WebAPI.Dtos
+{
+    public class RequestDto
+    {
+        public string Name { get; set; }
+        public string Duration { get; set; }
+        public string Stage { get; set; }
+    }
+}
